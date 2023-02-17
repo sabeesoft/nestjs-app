70989a63cef7793cd63a52b0dc77f246c0ddbc31
Varha Sabolch <vargha.sabolch@gmail.com>
2023-02-17 12:21:04 +0100

    app index

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,31 +6,9 @@ export class AppController {
 
   constructor(private readonly appService: AppService) { }
 
-  @Get("/list")
-  getAllHello(): string[] {
-    return this.appService.getAllHello();
+  @Get("/")
+  getWellcome() {
+    return this.appService.getWellcome();
   }
-
-  @Get("/test/:id")
-  getTest(@Param('id') id: string): Promise<{ result: string }> {
-    return this.appService.getTest(id);
-  }
-
-  @Post("/test")
-  createTest() {
-    return "Hello World!"
-  }
-
-  @Put("/test")
-  updateTest() {
-
-  }
-
-
-  @Delete("/test")
-  deleteTest() {
-
-  }
-
 }
 
